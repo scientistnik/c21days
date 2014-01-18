ab10ca1153aefc1f967a07bf3359e50310d4e8b8
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2014-01-19 01:19:03 +0400

    Exercise №1  Pleassekjc enter the commit message for your changes. Lines starting

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ CC=g++
 
 hellomake: hello.cpp
 	$(CC)	-o hello hello.cpp
+
+exercise1make: exercise1.cpp
+	$(CC) -o exercise1 exercise1.cpp
---
+++ exercise1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 5;
+	int y = 7;
+	cout << "\n";
+	cout << x + y << " " << x * y;
+	cout << "\n";
+	return 0;
+}
